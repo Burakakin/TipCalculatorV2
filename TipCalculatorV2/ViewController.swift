5070e3f1c97e148e8b4e467b0a5588ab36de8df1
Burak Akin <burakakin@buraks.local>
2018-07-12 16:51:03 +0300

    Initial Commit

---
+++ TipCalculatorV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculatorV2
+//
+//  Created by Burak Akin on 12.07.2018.
+//  Copyright © 2018 Burak Akin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
